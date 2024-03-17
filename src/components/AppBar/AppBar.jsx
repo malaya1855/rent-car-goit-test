ffca18acf374fe1ea1093b57d67537a52c773b3f
malaya1855 <121491813+malaya1855@users.noreply.github.com>
2024-03-17 18:11:35 +0200

    bckg image and header

--- src/components/AppBar/AppBar.jsx
+++ src/components/AppBar/AppBar.jsx
@@ -4,14 +4,16 @@ import headerLinks from "../../data/navHeader.json";
 
 export const AppBarMenu = () => {
   return (
-    <div className="container container-header">
-      <Navigation>
-        {headerLinks.map(({ id, title, href }) => (
-          <NavigationLink key={id} to={href}>
-            {title}
-          </NavigationLink>
-        ))}
-      </Navigation>
+    <div className="container-header">
+      <div className="container">
+        <Navigation>
+          {headerLinks.map(({ id, title, href }) => (
+            <NavigationLink key={id} to={href}>
+              {title}
+            </NavigationLink>
+          ))}
+        </Navigation>
+      </div>
     </div>
   );
 };
