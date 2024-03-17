66852bf80414507847207f8933a7ce8a2e8caa85
malaya1855 <121491813+malaya1855@users.noreply.github.com>
2024-03-17 16:01:24 +0000

    Deploying to gh-pages from @ malaya1855/rent-car-goit-test@77951c6d73ce47b4453857191e266453385700ac 🚀

--- assets/HomePage-zUBteNBg.js
+++ assets/HomePage-zUBteNBg.js
@@ -1,4 +1,4 @@
-import{u as r,j as e,L as a}from"./index-BDrAYPwj.js";import{m as n}from"./makes-DCESgvo9.js";const i=r.div`
+import{u as r,j as e,L as a}from"./index-CPhytsP8.js";import{m as n}from"./makes-DCESgvo9.js";const i=r.div`
   width: 100%;
   height: 400px;
   background-color: var(--secondary-text-color);
@@ -6,7 +6,7 @@ import{u as r,j as e,L as a}from"./index-BDrAYPwj.js";import{m as n}from"./makes
       rgba(18, 20, 23, 0.2),
       rgba(18, 20, 23, 0.2)
     ),
-    url("../../assets/images/hero.jpg");
+    url("../assets/images/hero.jpg");
   background-repeat: no-repeat;
   background-position: center;
   background-size: cover;
